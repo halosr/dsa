67909247b8ad1d86d6d180ab7773ba41708c2abe
Subhakanta Roul <papu.roul99@gmail.com>
2025-10-02 16:26:20 +0530

    ts: binary search recursion

--- TypeScript/algorithms/searching/binarySearch.ts
+++ TypeScript/algorithms/searching/binarySearch.ts
@@ -14,3 +14,19 @@ export default function binarySearch<T>(array: T[], key: T): number | null {
     }
     return null;
 }
+
+export function binarySearchRecursion<T>(array: T[], low: number, high: number, key: T): number | null {
+    if (low > high) {
+        return null;
+    }
+    let mid = Math.floor((low + high) / 2);
+    if (key === array[mid]) {
+        return mid;
+    } else if (key > array[mid]) {
+        low = mid + 1;
+        return binarySearchRecursion(array, low, high, key);
+    } else {
+        high = mid - 1;
+        return binarySearchRecursion(array, low, high, key);
+    }
+}
